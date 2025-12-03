842d8e1607956b9d3b29667e355bc6ef078f643a
bthu <bthu.fablab@gmail.com>
2025-12-03 11:57:37 +0700

    (qna) ui color animate

--- src/page_qna.py
+++ src/page_qna.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QThread
-from ui.widget_conf.ui_utils import SetStyleSheetForbtn
+from ui.widget_conf.ui_utils import SetStyleSheetForbtn, _animate_prompt
 from ui.main_ui import Ui_MainWindow
 from thread_speak import SpeakThread
 from thread_listen import ListenThread
@@ -24,7 +24,7 @@ class QnaPage:
         self.ui = main
         self.initial_context = initial_context
 
-        self.ui.btn_micro.clicked.connect(lambda: [self.listen(), self.ui.btn_home_qna.setEnabled(False), SetStyleSheetForbtn(self.ui, "btn_micro", "#69ff3d")])
+        self.ui.btn_micro.clicked.connect(lambda: [self.listen(), self.ui.btn_home_qna.setEnabled(False)])
         self.ui.btn_home_qna.clicked.connect(lambda: [self.ui.stackedWidget.setCurrentWidget(self.ui.page_main)])
 
         base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -41,47 +41,72 @@ class QnaPage:
         )
 
     def start_welcome(self):
-        SetStyleSheetForbtn(self.ui, "btn_speaker", "#69ff3d")
+        SetStyleSheetForbtn(self.ui, "btn_speaker", "#69ff3d", hover_background="#69ff3d")
         self.welcome_thread = WelcomeThread()
         self.welcome_thread.finished.connect(lambda: [self.cleanup_thread(self.welcome_thread), self.ui.btn_home_qna.setEnabled(True), self.ui.btn_micro.setEnabled(True),
-                                                    SetStyleSheetForbtn(self.ui, "btn_speaker", "#ffffff")])
+                                                    SetStyleSheetForbtn(self.ui, "btn_speaker", "#ffffff", hover_background="#ffffff")])
         self.welcome_thread.start()
 
     def listen(self):
         self.ui.btn_home_qna.setEnabled(False)
-        self.ui.prompt_qna.setText("Listening...")
+        self.start_animate("I’m listening", self.ui.prompt_qna)
+        SetStyleSheetForbtn(self.ui, "btn_micro", "#69ff3d", hover_background="#69ff3d")
         self.listen_thread = ListenThread()
         self.listen_thread.finished.connect(self.get_response)
-        self.listen_thread.finished.connect(lambda: [self.cleanup_thread(self.listen_thread), self.ui.btn_micro.setEnabled(False), 
-                                                    SetStyleSheetForbtn(self.ui, "btn_micro", "#ffffff"), SetStyleSheetForbtn(self.ui, "btn_speaker", "#69ff3d")])
+        # self.listen_thread.finished.connect(lambda: [self.cleanup_thread(self.listen_thread), self.ui.btn_micro.setEnabled(False), 
+        #                                             SetStyleSheetForbtn(self.ui, "btn_micro", "#ffffff", hover_background="#ffffff"), SetStyleSheetForbtn(self.ui, "btn_speaker", "#69ff3d", hover_background="#69ff3d")])
         self.listen_thread.start()
 
     def get_response(self, query: str):
+        self.stop_animate()
+        self.cleanup_thread(self.listen_thread)
+        self.ui.btn_micro.setEnabled(False)
+
         self.query = query
         if self.query == "Are you still there?" or self.query == "Hmm, I didn't quite catch that. Could you please repeat?" or self.query == "Something went wrong while listening." or self.query == "Speech service is unavailable.":
             print(f"AIko: {query}")
             self.ui.prompt_qna.setText(self.query)
+            SetStyleSheetForbtn(self.ui, "btn_micro", "#ffffff", hover_background="#ffffff")
+            SetStyleSheetForbtn(self.ui, "btn_speaker", "#69ff3d", hover_background="#69ff3d")
             self.speak_thread = SpeakThread(self.query)
             self.speak_thread.finished.connect(lambda: [self.cleanup_thread(self.speak_thread), self.ui.btn_micro.setEnabled(True), self.ui.btn_home_qna.setEnabled(True),
-                                                        SetStyleSheetForbtn(self.ui, "btn_speaker", "#ffffff")])
+                                                        SetStyleSheetForbtn(self.ui, "btn_speaker", "#ffffff", hover_background="#ffffff")])
             self.speak_thread.start()
         else:
             self.query = query.lower()
             print(f"You: {self.query}")
             self.response_thread = ResponseThread(self.query, chat_session=self.global_chat_session, initial_context=self.initial_context)
+            self.start_animate("I’m finding the best answer for you", self.ui.prompt_qna)
+            SetStyleSheetForbtn(self.ui, "btn_micro", "#ffffff", hover_background="#ffffff")
+            SetStyleSheetForbtn(self.ui, "btn_speaker", "#ffffff", hover_background="#ffffff")
+
             self.response_thread.finished.connect(self.answer)
             self.response_thread.finished.connect(lambda: [self.cleanup_thread(self.response_thread)])
             self.response_thread.start()
 
     def answer(self, text: str):
+        self.stop_animate()
         self.ui.prompt_qna.setText(text)
+        SetStyleSheetForbtn(self.ui, "btn_micro", "#ffffff", hover_background="#ffffff")
+        SetStyleSheetForbtn(self.ui, "btn_speaker", "#69ff3d", hover_background="#69ff3d")
         self.speak_thread = SpeakThread(text)
-        self.speak_thread.finished.connect(lambda: [self.cleanup_thread(self.speak_thread), SetStyleSheetForbtn(self.ui, "btn_speaker", "#ffffff"), 
+        self.speak_thread.finished.connect(lambda: [self.cleanup_thread(self.speak_thread), SetStyleSheetForbtn(self.ui, "btn_speaker", "#ffffff", hover_background="#ffffff"), 
                                                     self.ui.btn_home_qna.setEnabled(True), self.ui.btn_micro.setEnabled(True)])
         self.speak_thread.start()
+
 
     def cleanup_thread(self, thread: QThread):
         if thread:
             thread.quit()
             thread.wait()
             thread.deleteLater()
+
+    def start_animate(self, text, widget):
+        if hasattr(self, "stop_prompt") and self.stop_prompt:
+            self.stop_prompt()
+        self.stop_prompt = _animate_prompt(base_text=text, label_widget=widget)
+
+    def stop_animate(self):
+        if hasattr(self, "stop_prompt") and self.stop_prompt:
+            self.stop_prompt()
+            self.stop_prompt = None
